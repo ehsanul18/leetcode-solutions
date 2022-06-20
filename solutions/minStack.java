0340403b699f9dadcb73dbfc08555abceb51e721
Ehsanul Hoque <70466614+ehsanul18@users.noreply.github.com>
2022-06-20 09:36:31 +0600

    Create minStack.java

---
+++ solutions/minStack.java
@@ -0,0 +1,49 @@
+class MinStack {
+    
+    long min;
+    Stack<Long> stack;
+    
+    public MinStack() {
+        stack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()){
+            stack.push(0L);
+            min=val;
+        }else{
+            stack.push(val-min);//Could be negative if min value needs to change
+            if (val<min) min=val;
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) return;
+        
+        long pop=stack.pop();
+        
+        if (pop<0)  min=min-pop;//If negative, increase the min value
+    }
+    
+    public int top() {
+        long top=stack.peek();
+        if (top>0){
+            return (int)(top+min);
+        }else{
+           return (int)(min);
+        }
+    }
+    
+    public int getMin() {
+        return (int)min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
